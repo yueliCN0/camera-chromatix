e36d71362e76e26352f6ff220c7b3fc096713f8a
liyue <liyue@szkingsun.com>
2019-01-03 16:54:22 +0800

    CR# 备份初始效果代码

---
+++ camera-chromatix/P2_chromatix_gc2385_cxt/video/chromatix_gc2385_cxt_video.c
@@ -0,0 +1,27 @@
+/*============================================================================
+
+  Copyright (c) 2015 Qualcomm Technologies, Inc. All Rights Reserved.
+  Qualcomm Technologies Proprietary and Confidential.
+
+============================================================================*/
+
+/*============================================================================
+ *                      INCLUDE FILES
+ *===========================================================================*/
+#include "chromatix.h"
+#include "camera_dbg.h"
+
+static chromatix_parms_type chromatix_gc2385_cxt_parms = {
+#include "chromatix_gc2385_cxt_video.h"
+};
+
+/*============================================================================
+ * FUNCTION    - load_chromatix -
+ *
+ * DESCRIPTION:
+ *==========================================================================*/
+void *load_chromatix(void)
+{
+  CDBG("%s:%d%d\n", __func__, __LINE__);
+  return &chromatix_gc2385_cxt_parms;
+}
